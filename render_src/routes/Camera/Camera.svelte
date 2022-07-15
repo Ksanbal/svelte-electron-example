a253fd76f3a0461a263b47d87cf17110cd0b2e27
ksanbal <dev.ksanbal@gmail.com>
2022-07-15 14:23:23 +0900

    Camera 기능 적용

--- render_src/routes/Camera/Camera.svelte
+++ render_src/routes/Camera/Camera.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
   import { link } from "svelte-spa-router";
+
+  let videoSource: HTMLMediaElement | null = null;
+  let loading = true;
+
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+    })
+    .then((stream: MediaStream) => {
+      loading = false;
+      videoSource.srcObject = stream;
+    });
 </script>
 
 <h1>Camera!</h1>
-<a use:link href="/camera">camera</a>
+
+<a use:link href="/">Home</a>
+
+<div>
+  {#if loading}
+    <h1>Loading...</h1>
+  {/if}
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video autoplay bind:this={videoSource} />
+</div>
